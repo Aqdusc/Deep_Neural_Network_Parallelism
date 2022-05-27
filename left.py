4c17871161da92d3a17f7d966a94e4566674c14a
Aqdusc <54109303+Aqdusc@users.noreply.github.com>
2022-05-27 09:25:16 -0500

    Add files via upload

---
+++ left.py
@@ -0,0 +1,75 @@
+import os
+import torch.distributed.rpc as rpc
+import torch
+
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.nn.modules.container import Sequential
+import torch.nn as nn
+import torchvision.models as models
+
+os.environ['MASTER_ADDR'] = '134.193.129.157'
+os.environ['MASTER_PORT'] = '8880'
+
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
+
+
+r_50=models.resnet50()
+r_models=[r_50]
+for r in r_models:
+    net1=[]
+    for module in r.children():
+      if isinstance(module,Sequential):
+       for m in module.children():
+        net1.append(m)
+  
+      else:
+       net1.append(module)
+
+    len_net1=len(net1)
+
+
+split_point=8
+net1=nn.Sequential(*net1)
+left=list(net1.children())[:split_point]
+resnet_left=nn.Sequential(*left)
+resnet_right= list(net1.children())[split_point:-1]
+resnet_right = nn.Sequential(*[*resnet_right, Flatten(), list(net1.children())[-1]])
+
+device='cuda:0'
+
+
+img = Image.open('domestic-cat_thumb_square.jpg').convert('RGB')
+resize = transforms.Resize([224, 224])
+img = resize(img)
+to_tensor = transforms.ToTensor()
+tensor = to_tensor(img)
+tensor = tensor.unsqueeze(0)
+tensor=tensor.to(device)
+
+
+resnet_left=resnet_left.to(device)
+
+out_left=resnet_left(tensor)
+
+out_left=out_left.cpu()
+
+print(out_left)
+
+def my_script_add(t1, t2):
+  return out_left
+
+rpc.init_rpc("worker0", rank=0, world_size=2)
+print("init success")
+#ret = rpc.rpc_sync("worker1", my_script_add, args=(torch.ones(2), 3))
+#print(ret)
+print("worker1 success")
+rpc.shutdown()
